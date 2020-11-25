db53532ad18abcb8562ab0e5476ae085df574d1a
Dejan <dejan.sheki.lukic@gmail.com>
2020-11-25 14:33:13 +0100

    ...

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,9 +4,13 @@ $(function() {
         $(".bars").toggleClass('bars-open');
         $('.nav-menu').slideToggle(600);
     });
-    $('.nav-menu a').on("click", function() {
-        $('.nav-menu').slideToggle(600);
-        $(".bars-open").toggleClass('bars-open');
+
+    $('.nav-menu a').on('click', function() {
+        var ww = $(window).width();
+        if (ww < 768) {
+            $('.nav-menu').slideToggle(600);
+            $('.bars-open').toggleClass('bars-open');
+        }
     });
 
 });
